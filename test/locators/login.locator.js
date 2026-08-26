1fde14896a3c4c53c997638eeb0cb34fca319a81
AchmadFirmansyah55 <achmadfh.ibn@gmail.com>
2026-08-26 15:46:37 +0700

    second commit : add login locators

---
+++ test/locators/login.locator.js
@@ -0,0 +1,13 @@
+const { By } = require('selenium-webdriver');
+
+const LOGIN_LOCATORS = {
+    url:'https://www.saucedemo.com/',
+    selectors: {
+        usernameField : By.id('user-name'),
+        passwordField : By.id('password'),
+        loginButton : By.id('login-button'),
+        notification : By.xpath('//*[@data-test="error"]')
+    }
+};
+
+module.exports = LOGIN_LOCATORS;
